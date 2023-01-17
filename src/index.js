314a4d5c6b4efce41a25782496d376644e85ddf9
Jevin <therealjevin@gmail.com>
2023-01-17 10:32:00 -0600

    completed lab. kind of janky

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = "https://dog.ceo/api/breeds/list/all";
 
-
 fetchJSON(imgUrl).then(images => images.message.forEach(img => {
     const container = document.getElementById('dog-image-container');
     const ig = document.createElement('img');
@@ -11,8 +10,28 @@ fetchJSON(imgUrl).then(images => images.message.forEach(img => {
 
 fetchJSON(breedUrl).then(data => listBreeds(data.message));
 
+const selection = document.getElementById('breed-dropdown');
+selection.addEventListener('change', () => {
+    const userSelection = selection.options[selection.selectedIndex];
+    filterBreeds(userSelection.value);
+});
 function fetchJSON(url) {
     return fetch(url).then(response => response.json());
+}
+
+function filterBreeds(firstLetter) {
+    if(firstLetter === "undefined") {
+        return;
+    }
+    const list = Array.from(document.getElementById('dog-breeds').children);
+    list.forEach(li => {
+        if(li.textContent[0] === firstLetter) {
+            li.classList.remove('hidden');
+        }
+        else {
+            li.classList.add('hidden');
+        }
+    })
 }
 
 function listBreeds(breeds) {
